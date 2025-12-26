55a4db0d2d545b056aa1b5dac37c654a0c64a3ef
vanquocgit <job.vanquoc@gmail.com>
2025-12-26 11:51:16 +0700

    update data random img

--- JS/photos.js
+++ JS/photos.js
@@ -19,7 +19,7 @@ export async function addPhotoToScene(texture) {
     const group = new THREE.Group();
     group.add(frame); group.add(photo);
     frame.scale.set(width / 1.2, height / 1.2, 1);
-    const s = 0.8; group.scale.set(s, s, s);
+    const s = 1.5; group.scale.set(s, s, s);
     APP.photoMeshGroup.add(group);
     const mod = await import('./particles.js');
     APP.particleSystem.push(new mod.Particle(group, 'PHOTO', false));
@@ -29,8 +29,17 @@ export async function addPhotoToScene(texture) {
 export function loadPredefinedImages() {
     const loader = new THREE.TextureLoader();
     if (CONFIG.preload.autoScanLocal) {
-        for (let i = 1; i <= CONFIG.preload.scanCount; i++) {
-            const pathWEBP = `./IMG/${i}.webp`;
+        const count = Math.min(CONFIG.preload.scanCount || 35, 52);
+        // create shuffled sample of unique integers 1..52 and take `count`
+        const nums = Array.from({ length: 52 }, (_, i) => i + 1);
+        for (let i = nums.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+        }
+        const sample = nums.slice(0, count);
+
+        for (let i = 0; i < sample.length; i++) {
+            const pathWEBP = `./IMG/${sample[i]}.webp`;
             loader.load(pathWEBP, (t) => { t.colorSpace = THREE.SRGBColorSpace; addPhotoToScene(t); }, undefined, () => {
                 loader.load(pathWEBP, (t) => { t.colorSpace = THREE.SRGBColorSpace; addPhotoToScene(t); }, undefined, () => { });
             });
